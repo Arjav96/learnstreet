08685b000a190fb1b885278dcef5998345277486
yaman-jain <yamanjain007@gmail.com>
2013-06-24 23:59:32 -0700

    string_rot13-javascript commit

---
+++ ROT-13/string_rot13-javascript.js
@@ -0,0 +1,24 @@
+//ROT-13 Project
+function stringRot13(str) {
+    // Your Code Here
+    var rot13 = "";
+    var lower_case = "abcdefghijklmnopqrstuvwxyz";
+    var upper_case = lower_case.toUpperCase();
+    var loc;
+    for (var i = 0; i < str.length; ++i) {
+    if (str[i] == ' ') {
+        rot13 += ' ';
+    } else {
+        // Then check uppercase and lowercase
+        if (str[i] == str[i].toUpperCase()){
+        loc = upper_case.indexOf(str[i]);
+        rot13 += upper_case.charAt((loc+13)%26).toUpperCase();
+        } else {
+        loc = lower_case.indexOf(str[i]);
+        rot13 += lower_case.charAt((loc+13)%26);
+        }
+    }
+    }
+    return rot13;
+
+}
